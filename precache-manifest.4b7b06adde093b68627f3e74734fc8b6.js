4997b2fbce29afbbb76d6e5f580835a91c043027
Pulak <pulakkumarghosh2001@gmail.com>
2021-12-05 00:02:06 +0530

    Updates

---
+++ precache-manifest.4b7b06adde093b68627f3e74734fc8b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c54c33f816e6cb7e989b120eae3e45d",
+    "url": "/To-Buy_List/index.html"
+  },
+  {
+    "revision": "44bf330c2ff2ceab2296",
+    "url": "/To-Buy_List/static/css/main.a7e224c4.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/To-Buy_List/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "44bf330c2ff2ceab2296",
+    "url": "/To-Buy_List/static/js/main.da20451b.chunk.js"
+  },
+  {
+    "revision": "39ba9431f08ea136bec0",
+    "url": "/To-Buy_List/static/js/runtime~main.14267ee1.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/To-Buy_List/static/media/logo.ee7cd8ed.svg"
+  }
+]);
